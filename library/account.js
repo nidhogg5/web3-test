6739edc1bd13bc94bbae9f6ab995d89e7975904b
unknown <nidhogg5@yahoo.com.tw>
2017-12-20 03:00:00 +0800

    init

---
+++ library/account.js
@@ -0,0 +1,4 @@
+const andy = '0x05d828fa1430e0a729c4d61f8776315ee424cd7f'
+const ben = '0x7ebdb952cb6932087f2c012cac3e4aead4d37884'
+
+module.exports = {andy, ben}
---
+++ sendTransaction.js
@@ -0,0 +1,18 @@
+const web3 = require('./library/web3')
+const {andy, ben} = require('./library/account')
+
+web3.eth.sendTransaction({
+    from: andy,
+    to: ben,
+    value: 10,
+}).once('transactionHash', function (hash) {
+    console.log('交易送出 transaction hash : ', hash)
+}).once('receipt', function (receipt) {
+    console.log('交易被挖到了 receipt : ', receipt)
+}).on('confirmation', function (confNumber, receipt) {
+    console.log('交易穩定度 confNumber : ', confNumber)
+}).on('error', function (error) {
+    console.log('發生錯誤 error : ', error)
+}).then(function (receipt) {
+    console.log('交易完成 receipt : ', receipt)
+})
